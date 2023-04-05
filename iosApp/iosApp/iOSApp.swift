469822acebef6b5b877a10639b2f04e0250536ee
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-05 09:04:53 +0300

    refactor(app): use init instead of AppDelegate

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -3,18 +3,13 @@ import shared
 
 @main
 struct iOSApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    init() {
+        KoinKt.doInitKoin()
+    }
 
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
 		}
 	}
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        KoinKt.doInitKoin()
-        return true
-    }
 }
