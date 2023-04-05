37a5d811d2d9a873149dc27dfd882c11b0139adb
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-05 09:37:39 +0300

    feat: add expense repository interface

--- shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/repository/ExpenseRepostiory.kt
+++ shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/repository/ExpenseRepostiory.kt
@@ -4,10 +4,14 @@ import kotlinx.datetime.Clock
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
-class ExpenseRepository : KoinComponent {
+interface IExpenseRepository {
+    fun recordExpense(amount: Long)
+}
+
+class ExpenseRepository : KoinComponent, IExpenseRepository {
     private val database: Database by inject()
 
-    fun recordExpense(amount: Long) {
+    override fun recordExpense(amount: Long) {
         database.expenseQueries.insert(
             amount = amount,
             date = Clock.System.now().epochSeconds,
