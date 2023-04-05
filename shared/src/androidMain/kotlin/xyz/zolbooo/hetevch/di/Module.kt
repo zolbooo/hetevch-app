28a5b16fbd916003772a0152e2d82e7be957e9a2
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-05 09:58:02 +0300

    feat(di): provide MainDispatcher implementations

--- shared/src/androidMain/kotlin/xyz/zolbooo/hetevch/di/Module.kt
+++ shared/src/androidMain/kotlin/xyz/zolbooo/hetevch/di/Module.kt
@@ -2,8 +2,10 @@ package xyz.zolbooo.hetevch.di
 
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import xyz.zolbooo.hetevch.base.coroutines.MainDispatcher
 import xyz.zolbooo.hetevch.repository.DriverFactory
 
 actual fun platformModule(): Module = module {
     single { DriverFactory(get()) }
+    single { MainDispatcher() }
 }
