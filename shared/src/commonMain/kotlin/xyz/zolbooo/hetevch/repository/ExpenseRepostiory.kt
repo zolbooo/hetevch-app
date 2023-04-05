d8f198684fdcb1e2483f7794ebdbe81338deafc7
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-05 18:53:19 +0300

    feat: allow coroutine dispatcher to be injected to the expense repository

--- shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/repository/ExpenseRepostiory.kt
+++ shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/repository/ExpenseRepostiory.kt
@@ -2,6 +2,7 @@ package xyz.zolbooo.hetevch.repository
 
 import app.cash.sqldelight.coroutines.asFlow
 import app.cash.sqldelight.coroutines.mapToList
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
@@ -15,6 +16,7 @@ interface IExpenseRepository {
 
 class ExpenseRepository(
     private val database: Database,
+    private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : IExpenseRepository {
     override suspend fun recordExpense(amount: Long) {
         database.expenseQueries.recordExpense(
@@ -26,9 +28,7 @@ class ExpenseRepository(
     override suspend fun getAll(): Flow<List<Expenses>> =
         database.expenseQueries.selectAll()
             .asFlow()
-            // I'm not sure if we should use Dispatchers.IO or MainDispatcher,
-            // so for now we'll use Dispatchers.IO.
-            .mapToList(Dispatchers.IO)
+            .mapToList(coroutineDispatcher)
 
     override suspend fun removeExpense(id: Long) {
         database.expenseQueries.deleteExpense(id)
