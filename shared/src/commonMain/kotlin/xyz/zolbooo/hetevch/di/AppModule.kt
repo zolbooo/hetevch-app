3e790274ba0b03177961a70b8e2611b2b3c359ca
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-05 10:01:25 +0300

    feat(di): add repository module

--- shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/di/AppModule.kt
+++ shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/di/AppModule.kt
@@ -3,15 +3,20 @@ package xyz.zolbooo.hetevch.di
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
-import xyz.zolbooo.hetevch.repository.createDatabase
+import xyz.zolbooo.hetevch.repository.*
 
 val appModule = module {
     single { createDatabase(get()) }
+}
+val repositoryModule = module {
+    single<IBudgetRepository> { BudgetRepository(get()) }
+    single<IExpenseRepository> { ExpenseRepository(get()) }
 }
 
 fun initKoin(appDeclaration: KoinAppDeclaration = {}) = startKoin {
     appDeclaration()
     modules(
+        repositoryModule,
         appModule,
         platformModule(),
     )
