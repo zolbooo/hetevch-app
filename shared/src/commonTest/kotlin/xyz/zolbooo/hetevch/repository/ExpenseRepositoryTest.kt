6309984533acb3adfa6abd796d7d9a8c39ae290d
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-05 19:14:58 +0300

    test: expense repository

---
+++ shared/src/commonTest/kotlin/xyz/zolbooo/hetevch/repository/ExpenseRepositoryTest.kt
@@ -0,0 +1,88 @@
+package xyz.zolbooo.hetevch.repository
+
+import app.cash.sqldelight.db.SqlDriver
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import xyz.zolbooo.hetevch.RobolectricTests
+import kotlin.test.*
+
+class ExpenseRepositoryTest : RobolectricTests() {
+    private lateinit var databaseDriver: SqlDriver
+    private lateinit var database: Database
+
+    private lateinit var budgetRepository: BudgetRepository
+    private lateinit var expenseRepository: ExpenseRepository
+
+    @BeforeTest
+    fun setup() {
+        databaseDriver = getTestDriverFactory().createDriver()
+        database = Database(databaseDriver)
+
+        budgetRepository = BudgetRepository(database)
+        expenseRepository = ExpenseRepository(database)
+    }
+
+    @AfterTest
+    fun teardown() {
+        database.expenseQueries.deleteAll()
+        database.budgetQueries.deleteAll()
+        databaseDriver.close()
+    }
+
+    @Test
+    fun recordExpense() = runTest {
+        budgetRepository.setBudget(10_000, 3)
+
+        expenseRepository.recordExpense(5_000)
+
+        val budget = budgetRepository.getLatest()
+        assertNotNull(budget)
+        assertEquals(5_000, budget.balance)
+    }
+
+    @Test
+    fun removeExpense() = runTest {
+        budgetRepository.setBudget(10_000, 3)
+
+        expenseRepository.recordExpense(5_000)
+        expenseRepository.recordExpense(4_000)
+
+        val expenses = expenseRepository.getAll()
+        expenseRepository.removeExpense(expenses[1].id)
+
+        val updatedExpenses = expenseRepository.getAll()
+        assertEquals(updatedExpenses[0].amount, 4_000)
+        val budget = budgetRepository.getLatest()
+        assertEquals(6_000, budget!!.balance)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun watchAllExpenses() = runTest {
+        val testExpenseRepository = ExpenseRepository(
+            database,
+            UnconfinedTestDispatcher(testScheduler),
+        )
+        budgetRepository.setBudget(10_000, 3)
+
+        val expenses = mutableListOf<List<Expenses>>()
+        backgroundScope.launch {
+            testExpenseRepository.watchAll().toList(expenses)
+        }
+        // Add delays to allow Flow to write intermediate values to the list
+        testExpenseRepository.recordExpense(500)
+        delay(500)
+        testExpenseRepository.recordExpense(1000)
+        delay(500)
+
+        assertEquals(2, expenses.size)
+        assertEquals(500, expenses[0][0].amount)
+
+        assertEquals(500, expenses[1][1].amount)
+        assertEquals(1000, expenses[1][0].amount)
+    }
+}
