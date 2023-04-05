5cd2933d9464f837d628b63a74c00b01f8e9abf5
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-05 14:06:15 +0300

    fix(budget-queries-test): close database driver after tests

--- shared/src/commonTest/kotlin/xyz/zolbooo/hetevch/repository/BudgetQueriesTest.kt
+++ shared/src/commonTest/kotlin/xyz/zolbooo/hetevch/repository/BudgetQueriesTest.kt
@@ -1,19 +1,23 @@
 package xyz.zolbooo.hetevch.repository
 
+import app.cash.sqldelight.db.SqlDriver
 import xyz.zolbooo.hetevch.RobolectricTests
 import kotlin.test.*
 
 class BudgetQueriesTest : RobolectricTests() {
+    private lateinit var databaseDriver: SqlDriver
     private lateinit var database: Database
 
     @BeforeTest
     fun setup() {
-        database = createDatabase(getTestDriverFactory())
+        databaseDriver = getTestDriverFactory().createDriver()
+        database = Database(databaseDriver)
     }
 
     @AfterTest
     fun teardown() {
         database.budgetQueries.deleteAll()
+        databaseDriver.close()
     }
 
     @Test
