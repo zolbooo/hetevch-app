b9a0f9885b5fd86df639fb4a7906cd65af1c7344
Bazarsad Uurdmandakh <ubazraa3@gmail.com>
2023-04-07 11:28:38 +0800

    Initial Commit

---
+++ Hetevch/HetevchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HetevchApp.swift
+//  Hetevch
+//
+//  Created by Bazarsad Uurdmandakh on 2023.04.07.
+//
+
+import SwiftUI
+
+@main
+struct HetevchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
