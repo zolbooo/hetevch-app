116758993867d1f8bb3e81fe9bf1f86d331590a5
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-11 20:26:47 +0300

    feat(android-ui): add budget widget

---
+++ androidApp/src/main/java/xyz/zolbooo/hetevch/android/widgets/BudgetWidget.kt
@@ -0,0 +1,115 @@
+package xyz.zolbooo.hetevch.android.widgets
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import xyz.zolbooo.hetevch.android.R
+import xyz.zolbooo.hetevch.android.components.BudgetCard
+import xyz.zolbooo.hetevch.android.ui.HetevchTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BudgetWidget(onSave: (Long, Int) -> Unit, modifier: Modifier = Modifier) {
+    var amount by remember { mutableStateOf("") }
+    var duration by remember { mutableStateOf<Int?>(null) }
+    Column(modifier.padding(20.dp)) {
+        BudgetCard(amount = 0)
+        Spacer(Modifier.height(20.dp))
+        Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+            OutlinedTextField(
+                label = { Text(text = stringResource(R.string.budget_amount)) },
+                value = amount,
+                onValueChange = { amount = it },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Decimal,
+                    imeAction = ImeAction.Next,
+                ),
+                modifier = Modifier.fillMaxWidth(),
+            )
+
+            var expanded by remember { mutableStateOf(false) }
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = { expanded = !expanded },
+            ) {
+                OutlinedTextField(
+                    label = { Text(text = stringResource(R.string.budget_duration)) },
+                    value = if (duration == null) "" else "--",
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                    modifier = Modifier
+                        .menuAnchor()
+                        .fillMaxWidth(),
+                )
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false },
+                ) {
+                    repeat(45) {
+                        DropdownMenuItem(
+                            text = { Text(it.toString()) },
+                            onClick = {
+                                duration = it
+                                expanded = false
+                            },
+                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                        )
+                    }
+                }
+            }
+
+            OutlinedTextField(
+                label = { Text(stringResource(R.string.budget_daily_amount)) },
+                value = "",
+                onValueChange = {},
+                enabled = false,
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+        Button(
+            onClick = {
+                val amountAsLong = amount.toLongOrNull()
+                if (amountAsLong != null) {
+                    duration?.let {
+                        onSave(amountAsLong, it)
+                    }
+                }
+            },
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .fillMaxWidth(),
+        ) {
+            Text(stringResource(R.string.budget_save))
+        }
+    }
+}
+
+@Preview(name = "Light mode")
+@Composable
+fun BudgetWidgetPreview() {
+    HetevchTheme {
+        Surface {
+            BudgetWidget(onSave = { _, _ -> })
+        }
+    }
+}
+
+@Preview(name = "Dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun BudgetWidgetPreviewDarkMode() {
+    HetevchTheme {
+        Surface {
+            BudgetWidget(onSave = { _, _ -> })
+        }
+    }
+}
