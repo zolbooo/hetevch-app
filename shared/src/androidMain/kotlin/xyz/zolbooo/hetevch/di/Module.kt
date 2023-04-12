8406671872478a2a1cbc7a92e454e639907be95b
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-12 20:44:44 +0300

    feat(di): add Settings singletons for platforms

--- shared/src/androidMain/kotlin/xyz/zolbooo/hetevch/di/Module.kt
+++ shared/src/androidMain/kotlin/xyz/zolbooo/hetevch/di/Module.kt
@@ -1,5 +1,7 @@
 package xyz.zolbooo.hetevch.di
 
+import android.content.Context
+import com.russhwolf.settings.SharedPreferencesSettings
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import xyz.zolbooo.hetevch.base.coroutines.MainDispatcher
@@ -7,5 +9,12 @@ import xyz.zolbooo.hetevch.repository.DriverFactory
 
 actual fun platformModule(): Module = module {
     single { DriverFactory(get()) }
+    single {
+        val sharedPreferences = get<Context>().getSharedPreferences(
+            "xyz.zolbooo.hetevch.SETTINGS",
+            Context.MODE_PRIVATE
+        )
+        SharedPreferencesSettings(sharedPreferences)
+    }
     single { MainDispatcher() }
 }
--- shared/src/iosMain/kotlin/xyz/zolbooo/hetevch/di/Module.kt
+++ shared/src/iosMain/kotlin/xyz/zolbooo/hetevch/di/Module.kt
@@ -1,11 +1,14 @@
 package xyz.zolbooo.hetevch.di
 
+import com.russhwolf.settings.NSUserDefaultsSettings
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import platform.Foundation.NSUserDefaults
 import xyz.zolbooo.hetevch.base.coroutines.MainDispatcher
 import xyz.zolbooo.hetevch.repository.DriverFactory
 
 actual fun platformModule(): Module = module {
     single { DriverFactory() }
+    single { NSUserDefaultsSettings(NSUserDefaults.standardUserDefaults) }
     single { MainDispatcher() }
 }
