5a4794bb0bc7b5759544001ebf52c0e35286970d
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-14 20:42:35 +0300

    feat(create-budget-screen): use scaffold

--- androidApp/src/main/java/xyz/zolbooo/hetevch/android/navigation/screens/CreateBudgetScreen.kt
+++ androidApp/src/main/java/xyz/zolbooo/hetevch/android/navigation/screens/CreateBudgetScreen.kt
@@ -1,16 +1,24 @@
 package xyz.zolbooo.hetevch.android.navigation.screens
 
 import android.util.Log
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import xyz.zolbooo.hetevch.android.ui.widgets.BudgetWidget
 
 fun NavGraphBuilder.createBudgetScreen() {
+    @OptIn(ExperimentalMaterial3Api::class)
     composable("create-budget") {
-        BudgetWidget(
-            onSave = { amount, durationInDays ->
-                Log.d("BudgetWidget", "Save: $amount, duration is $durationInDays days")
-            },
-        )
+        Scaffold { paddingValues ->
+            BudgetWidget(
+                onSave = { amount, durationInDays ->
+                    Log.d("BudgetWidget", "Save: $amount, duration is $durationInDays days")
+                },
+                modifier = Modifier.padding(paddingValues)
+            )
+        }
     }
 }
