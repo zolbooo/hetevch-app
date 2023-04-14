3edc00112980f3a30415faa23208be7bc6a78ca0
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-14 20:24:06 +0300

    feat(android): compute start destination

--- androidApp/src/main/java/xyz/zolbooo/hetevch/android/MainActivity.kt
+++ androidApp/src/main/java/xyz/zolbooo/hetevch/android/MainActivity.kt
@@ -6,14 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import xyz.zolbooo.hetevch.android.ui.HetevchTheme
 import xyz.zolbooo.hetevch.android.navigation.NavigationRoot
+import xyz.zolbooo.hetevch.helpers.BudgetStatus
+import xyz.zolbooo.hetevch.helpers.InitHelper
 
 class MainActivity : ComponentActivity() {
+    private val initHelper = InitHelper()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+
+        // Compute value for startDestination here as the splash screen is being shown
+        val startDestination = when (initHelper.getBudgetStatus()) {
+            BudgetStatus.NotCreated -> "create-budget"
+            BudgetStatus.MoneySaved -> "money-saved"
+            BudgetStatus.Ongoing -> "home"
+            BudgetStatus.Ended -> "budget-ended"
+        }
         setContent {
             HetevchTheme {
-                NavigationRoot(startDestination = "home")
+                NavigationRoot(startDestination = startDestination)
             }
         }
     }
---
+++ shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/helpers/InitHelper.kt
@@ -0,0 +1,49 @@
+package xyz.zolbooo.hetevch.helpers
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import xyz.zolbooo.hetevch.repository.IBudgetRepository
+import xyz.zolbooo.hetevch.repository.ISettingsRepository
+
+sealed class BudgetStatus {
+    /**
+     * There is some money saved from previous days.
+     */
+    object MoneySaved : BudgetStatus()
+
+    /**
+     * Budget is ongoing, just open the home screen.
+     */
+    object Ongoing : BudgetStatus()
+
+    /**
+     * Budget is yet to be created.
+     */
+    object NotCreated : BudgetStatus()
+
+    /**
+     * The budget has ended, user has to create to a new one.
+     */
+    object Ended : BudgetStatus()
+}
+
+class InitHelper : KoinComponent {
+    private val budgetRepository by inject<IBudgetRepository>()
+    private val settingsRepository by inject<ISettingsRepository>()
+
+    fun getBudgetStatus(): BudgetStatus {
+        val budget = budgetRepository.getLatest() ?: return BudgetStatus.NotCreated
+
+        val timezone = TimeZone.currentSystemDefault()
+        val today = Clock.System.now().toLocalDateTime(timezone).date
+        val lastOpenDate = settingsRepository.getLastOpenDate().toLocalDateTime(timezone).date
+        return when {
+            budget.end >= today -> BudgetStatus.Ended
+            lastOpenDate < today -> BudgetStatus.MoneySaved
+            else -> BudgetStatus.Ongoing
+        }
+    }
+}
