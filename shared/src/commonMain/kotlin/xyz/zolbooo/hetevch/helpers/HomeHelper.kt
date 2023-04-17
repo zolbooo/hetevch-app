ef474ed4f868dbb5ea23bf25708c7c67e002ec5e
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-17 19:21:37 +0300

    feat(home-helper): implement getRemainingDaysForBudget method

--- shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/helpers/HomeHelper.kt
+++ shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/helpers/HomeHelper.kt
@@ -1,10 +1,15 @@
 package xyz.zolbooo.hetevch.helpers
 
-import kotlinx.datetime.Clock
+import kotlinx.datetime.*
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import xyz.zolbooo.hetevch.repository.Budget
 import xyz.zolbooo.hetevch.repository.IBudgetRepository
+
+fun Budget.calculateRemainingDays(clock: Clock): Int {
+    val today = clock.now().toLocalDateTime(TimeZone.UTC).date
+    return end.minus(today).days + 1
+}
 
 class HomeHelper : KoinComponent {
     private val clock by inject<Clock>()
@@ -12,7 +17,5 @@ class HomeHelper : KoinComponent {
     private val budgetRepository by inject<IBudgetRepository>()
 
     fun getBudget() = budgetRepository.getLatest()!!
-    fun getRemainingDaysForBudget(budget: Budget): Int {
-        return TODO("Not implemented")
-    }
+    fun getRemainingDaysForBudget(budget: Budget): Int = budget.calculateRemainingDays(clock)
 }
---
+++ shared/src/commonTest/kotlin/xyz/zolbooo/hetevch/helpers/HomeHelperTest.kt
@@ -0,0 +1,27 @@
+package xyz.zolbooo.hetevch.helpers
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDate
+import xyz.zolbooo.hetevch.repository.Budget
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class HomeHelperTest {
+    @Test
+    fun calculateRemainingDays() {
+        val clock = object : Clock {
+            override fun now(): Instant {
+                return Instant.parse("2023-01-01T14:00:00.00Z")
+            }
+        }
+        assertEquals(
+            3,
+            Budget(
+                amount = 1,
+                dailyAmount = 1,
+                end = LocalDate.parse("2023-01-03"),
+            ).calculateRemainingDays(clock)
+        )
+    }
+}
