2e3850f3828e1d6cb9bfc91ae42bba839570e943
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-17 14:47:54 +0300

    fix(budget-helper): inject using interface

--- shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/helpers/BudgetHelper.kt
+++ shared/src/commonMain/kotlin/xyz/zolbooo/hetevch/helpers/BudgetHelper.kt
@@ -2,10 +2,10 @@ package xyz.zolbooo.hetevch.helpers
 
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import xyz.zolbooo.hetevch.repository.BudgetRepository
+import xyz.zolbooo.hetevch.repository.IBudgetRepository
 
 class CreateBudgetHelper : KoinComponent {
-    private val budgetRepository by inject<BudgetRepository>()
+    private val budgetRepository by inject<IBudgetRepository>()
 
     fun setBudget(amount: Long, duration: Int) = budgetRepository.setBudget(amount, duration)
 }
