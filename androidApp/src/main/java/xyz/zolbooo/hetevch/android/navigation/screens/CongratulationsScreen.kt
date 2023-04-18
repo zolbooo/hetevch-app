57259165f2e431477d537a321eef68301feb1139
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-18 17:31:52 +0300

    feat(congratulations-screen): show calculated saving values

--- androidApp/src/main/java/xyz/zolbooo/hetevch/android/navigation/screens/CongratulationsScreen.kt
+++ androidApp/src/main/java/xyz/zolbooo/hetevch/android/navigation/screens/CongratulationsScreen.kt
@@ -1,17 +1,23 @@
 package xyz.zolbooo.hetevch.android.navigation.screens
 
+import androidx.compose.runtime.remember
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import xyz.zolbooo.hetevch.android.ui.screens.CongratulationsScreen
+import xyz.zolbooo.hetevch.helpers.MoneySavingHelper
 
 fun NavGraphBuilder.congratulationsScreen(navController: NavController) {
     composable("money-saved") {
+        val moneySavingHelper = remember { MoneySavingHelper() }
+        val budget = remember { moneySavingHelper.getBudget() }
+        val savedAmount = remember { moneySavingHelper.getSavedMoneyAmount() }
+        val remainingDays = remember { moneySavingHelper.getRemainingDaysForBudget(budget) }
         CongratulationsScreen(
-            savedAmount = 0,
-            estimatedNewBudgetAmount = 0,
-            budgetDurationInDays = 0,
-            estimatedNewTodayBudgetAmount = 0,
+            savedAmount = savedAmount,
+            estimatedNewBudgetAmount = budget.amount / remainingDays,
+            budgetDurationInDays = remainingDays,
+            estimatedNewTodayBudgetAmount = budget.dailyAmount + savedAmount,
             onAddToTotalBudgetPress = { /*TODO*/ },
             onAddToDailyBudgetPress = { /*TODO*/ },
             onSavePress = {
