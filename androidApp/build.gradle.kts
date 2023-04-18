fbc861a5c4296146d3d1cb4139842ad49016e18c
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-18 20:09:40 +0300

    chore: app preview release 0.1.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "xyz.zolbooo.hetevch.android"
         minSdk = 24
         targetSdk = 33
-        versionCode = 2
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.1.1"
     }
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
