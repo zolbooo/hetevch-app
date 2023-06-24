e912fd79ae95f8f132c22851de8ac36987d35517
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-06-24 14:18:16 +0300

    feat(ios): add CreateBudgetView UI

--- iosApp/iosApp/Screens/CreateBudgetView.swift
+++ iosApp/iosApp/Screens/CreateBudgetView.swift
@@ -10,15 +10,42 @@ import SwiftUI
 import SVGView
 
 struct CreateBudgetView: View {
+    @State private var amount = ""
+    @State private var endDate = Date()
+    
+    private let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.minimum = .init(integerLiteral: 1)
+        formatter.maximum = .init(integerLiteral: Int.max)
+        formatter.generatesDecimalNumbers = false
+        formatter.maximumFractionDigits = 0
+        return formatter
+    }()
     var body: some View {
-        VStack {
+        Form {
             BudgetCardView(amount: "₮ 380,000.00")
-        }.padding()
+            Section(header: Text("Төсөв")) {
+                TextField("Хэмжээ", value: $amount, formatter: numberFormatter)
+                DatePicker(selection: $endDate, in: Date()..., displayedComponents: .date) { Text("Дуусах өдөр") }
+            }
+            Section {
+                HStack {
+                    Text("Өдрийн төсөв")
+                    Spacer()
+                    Text("₮ 38,000.00")
+                }
+                Button(action: {}) {
+                    Text("Хадгалах")
+                }
+            }
+        }.navigationTitle("Төсөв үүсгэх")
     }
 }
 
 struct CreateBudgetView_Previews: PreviewProvider {
     static var previews: some View {
-        CreateBudgetView()
+        NavigationView {
+            CreateBudgetView()
+        }
     }
 }
